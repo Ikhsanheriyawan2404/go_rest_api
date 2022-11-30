d83f413c988a42162e2ae1f1696770ca25e195df
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2022-11-30 14:24:11 +0700

    Feat Add Controller Category

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -7,14 +7,20 @@ import (
 	"golang_rest_api/model/domain"
 	"golang_rest_api/model/web"
 	"golang_rest_api/repository"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	validate *validator.Validate
 }
 
 func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
@@ -28,6 +34,9 @@ func (service *CategoryServiceImpl) Create(ctx context.Context, request web.Cate
 }
 
 func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.validate.Struct(request)
+	helper.PanicIfError(err)
+	
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
